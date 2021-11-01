593782275419df5a250e03ac904b5b972f5ff0c7
Robert Pliszka <robpliszka@gmail.com>
2021-10-31 20:37:07 -0400

    Added another sample of average function with 2 arguments

--- function_sample.py
+++ function_sample.py
@@ -4,10 +4,19 @@
 def add_sum(*args):
     return sum(args)
 
+# This function does a average of multiple arguments being passed into the function
+
 
 def average_sum(*args):
     return sum(args) / len(args)
 
+# This is a function that take 2 arguments.
+
+
+def two_number_average(a, b):
+    return a + b
+
 
 print(add_sum(1, 4, 5, 6, 7))
 print(average_sum(3, 5, 6, 7, 10, 33))
+print(two_number_average(1, 7))
